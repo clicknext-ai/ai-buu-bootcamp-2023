4650757ac0a86b46bd0dbea4d033ea3fbb08caf5
SupavitClickNext <153151363+SupavitClickNext@users.noreply.github.com>
2023-12-20 16:01:54 +0700

    modify 2 files for Vit session BUU bootcamp

--- inference.py
+++ inference.py
@@ -4,10 +4,4 @@ from ultralytics import YOLO
 model = YOLO(r"{YOUR_FILE_PATH_best.pt}")
 
 # Run inference on 'bus.jpg' with arguments
-model.track(
-    source=r"{FOLDER_CONTAIN_JPG_IMAGE_FILE}/*.jpg",
-    save=True,
-    imgsz=640,
-    conf=0.5,
-    show=False,
-)
+model.track(source=r"{FOLDER_CONTAIN_JPG_IMAGE_FILE}/*.jpg", save=True, imgsz=640, conf=0.5, show=False)
--- train_custom.py
+++ train_custom.py
@@ -1,20 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8m.yaml").load("yolov8m.pt")  # build from YAML and transfer weights
+model = YOLO('yolov8m.yaml').load('yolov8m.pt')  # build from YAML and transfer weights
 
 # Train the model
 custom_yaml = r"{YOUR_PATH_data.yaml}"
-results = model.train(
-    data=custom_yaml,
-    epochs=1000,
-    imgsz=640,
-    batch=8,
-    workers=1,
-    plots=True,
-    device=0,
-    patience=50,
-    flipud=0.5,
-    mixup=0.7,
-    name=r"{NAME_OF_FOLDER_TO_SAVE_MODEL}",
-)
+results = model.train(data=custom_yaml, epochs=1000, imgsz=640, batch=8, workers=1, plots=True, device=0, patience=50, 
+                      flipud=0.5, mixup=0.7,
+                      name=r"{NAME_OF_FOLDER_TO_SAVE_MODEL}")
